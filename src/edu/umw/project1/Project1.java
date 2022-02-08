ff592cecfb28744cb21114fb571d8559a36d0045
SykoDK <94087255+SykoDK@users.noreply.github.com>
2022-02-07 20:55:25 -0500

    Tried using for loop. Seeing where it goes from here.

---
+++ src/edu/umw/project1/Project1.java
@@ -0,0 +1,35 @@
+/**
+ * CPSC 220
+ * Project 1 - Finding Factors
+ * I pledge
+ * Ian Grzebien
+ *
+ */
+package edu.umw.project1;
+import java.util.Scanner;
+
+public class Project1 {
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the Factor Factory");
+        System.out.println("Enter the lowest number in the range to be factored > ");
+        int lowRange = input.nextInt();
+        System.out.println("Enter the highest number in the range to be factored > ");
+        int highRange = input.nextInt();
+
+        for (int i = lowRange; i <= highRange; ++i){
+            System.out.println("");
+        }
+
+        System.out.println("Goodbye!");
+    }
+    static String findFactors(int num) {
+
+
+
+        return null;
+    }
+
+
+}
